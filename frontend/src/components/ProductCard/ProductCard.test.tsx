cc3c38582c615ffe761af6aab005a6203ad5ece4
clarencelindh@gmail.com <clarencelindh@gmail.com>
2022-07-04 14:26:27 +0200

    product card plus tests

---
+++ frontend/src/components/ProductCard/ProductCard.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../store/store";
+import ProductCard from "./ProductCard";
+
+
+describe('Product card', () => {
+  
+  it('checks if product name is rendered', () => {
+    render(
+      <Provider store={store}>
+        <ProductCard product={{
+          id: 1,
+          name: "Test product 1",
+          price: 10
+        }} />
+      </Provider>
+    );
+    
+    const inputElement = screen.getByText("Test product 1")
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it('checks if product price is rendered', () => {
+    render(
+      <Provider store={store}>
+        <ProductCard product={{
+          id: 1,
+          name: "Test product 1",
+          price: 10
+        }} />
+      </Provider>
+    );
+    
+    const inputElement = screen.getByText("10 kr")
+    expect(inputElement).toBeInTheDocument();
+  });
+
+});
